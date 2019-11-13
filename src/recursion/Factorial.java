e6dbfe361fe9ea2f291d74ee857b85ad9d3ba6b4
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-13 19:19:34 +0530

    complete

--- src/recursion/Factorial.java
+++ src/recursion/Factorial.java
@@ -1,7 +1,9 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Factorial {
-    public int factorial(int a) {
+    public static int factorial(int a) {
         int fact = 1;
         if (a == 0) {
             return 1;
@@ -9,5 +11,12 @@ public class Factorial {
             fact = a * factorial(a - 1);
             return fact;
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a no.:");
+        int number = scan.nextInt();
+        System.out.println(Factorial.factorial(number));
     }
 }
