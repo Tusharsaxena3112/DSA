9850299a9ef38ee1a92079cb9ab888aad4f07dad
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-13 19:08:37 +0530

    adding sum method in Sum.java

--- src/recursion/Sum.java
+++ src/recursion/Sum.java
@@ -2,7 +2,13 @@ package recursion;
 
 public class Sum {
     public int sum(int a) {
-
-
+        int s = 0;
+        if (a == 1) {
+            return a;
+        } else {
+            s = a + sum(a - 1);
+            return s;
+        }
     }
+
 }
