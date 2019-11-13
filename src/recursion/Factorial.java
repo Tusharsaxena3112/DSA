0fa2816ce450ce3962084da5e810743750fcaadd
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-13 19:16:19 +0530

    adding Factorial.java

---
+++ src/recursion/Factorial.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class Factorial {
+    public int factorial(int a) {
+        int fact = 1;
+        if (a == 0) {
+            return 1;
+        } else {
+
+        }
+    }
+}
