f69d97e86b33b84a8bfbfeeb8fc2e5699c279a75
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-19 00:13:30 +0530

    adding main method to StackExecution.java

--- src/stack/StackExecution.java
+++ src/stack/StackExecution.java
@@ -1,5 +1,9 @@
 package stack;
 
 public class StackExecution {
-
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+        stack.push(2);
+        stack.showStack();
+    }
 }
