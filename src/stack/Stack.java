df76fef089c589dcfd175ee2e2f00a4645daf57b
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-19 00:02:56 +0530

    adding constructor to Stack.java

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -3,5 +3,18 @@ package stack;
 public class Stack {
     private int top = 0;
     private int stackSize;
+    private int[] stack = new int[stackSize];
 
+    public Stack(int stackSize) {
+        this.stackSize = stackSize;
+    }
+
+    public void push(int argument) {
+        if (top > stackSize) {
+            System.out.println("Overflow");
+        } else {
+
+        }
+
+    }
 }
