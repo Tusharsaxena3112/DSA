9bc6075d9c7c8f8fbf8f6c1538ff64337116b5c3
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-01 23:40:41 +0530

    adding BubbleSortApp.java

--- src/sortingAlgorithms/BubbleSortApp.java
+++ src/sortingAlgorithms/BubbleSortApp.java
@@ -1,6 +1,36 @@
 package sortingAlgorithms;
 
-public class BubbleSortApp {
+public class BubbleSortApp<T> {
+    public int length;
+    public int[] array;
+    public int index = 0;
 
+    public BubbleSortApp(int[] array, int length) {
+        this.length = length;
+        this.array = array;
+    }
 
+    public void insert(int element) {
+        array[this.index] = element;
+        this.index++;
+    }
+
+    public void bubbleSort() {
+        int temp = 0;
+        for (int i = array.length - 1; i > 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+    }
 }
