5bf327657ff06ca365dd05d279e76d258d91269f
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-02 16:47:57 +0530

    modifying InsertionSortApp.java

--- src/sortingAlgorithms/selectionSort/InsertionSortApp.java
+++ src/sortingAlgorithms/selectionSort/InsertionSortApp.java
@@ -20,8 +20,8 @@ public class InsertionSortApp {
         for (int i = 1; i < array.length; i++) {
             temp = array[i];
             j = i;
-            while (j > 0 && array[j] < array[j - 1]) {
-                array[j - 1] = array[j];
+            while (j > 0 && array[j - 1] > temp) {
+                array[j] = array[j - 1];
                 j--;
             }
             array[j] = temp;
