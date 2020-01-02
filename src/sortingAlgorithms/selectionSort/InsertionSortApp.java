1fb164db6e4d5ea6cb4ced47b077ae20f470cc56
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-02 16:06:29 +0530

    completing InsertionSortApp.java

--- src/sortingAlgorithms/selectionSort/InsertionSortApp.java
+++ src/sortingAlgorithms/selectionSort/InsertionSortApp.java
@@ -3,8 +3,34 @@ package sortingAlgorithms.selectionSort;
 public class InsertionSortApp {
     public int[] array;
     public int length;
+    public int index = 0;
 
-    public InsertionSortApp() {
+    public InsertionSortApp(int length) {
+        this.length = length;
+        array = new int[length];
+    }
 
+    public void insert(int element) {
+        array[index] = element;
+        index++;
+    }
+
+    public void insertSort() {
+        int temp = 0, j;
+        for (int i = 1; i < array.length; i++) {
+            temp = array[i];
+            j = i;
+            while (j > 0 && array[j] < array[j - 1]) {
+                array[j - 1] = array[j];
+                j--;
+            }
+            array[j] = temp;
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
