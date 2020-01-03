5710884454032cb6243085dd98bb47f04312e158
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-03 11:32:44 +0530

    adding Queue.java

---
+++ src/queue/Queue.java
@@ -0,0 +1,13 @@
+package queue;
+
+public class Queue {
+    private int length;
+    private int front;
+    private int rear;
+
+    public Queue(int length) {
+        this.length = length;
+    }
+
+
+}
