439446cf2067dfcde693a3fe05942e8f5665105e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-04 13:32:11 +0530

    adding LinkedList.java

---
+++ src/linkedList/Execution.java
@@ -0,0 +1,10 @@
+package linkedList;
+
+public class Execution {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addLast(4);
+        linkedList.addLast(5);
+
+    }
+}
---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,29 @@
+package linkedList;
+
+public class LinkedList {
+    private Node first;
+    private Node last;
+
+    public void addLast(int value) {
+        Node node = new Node(value);
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node;
+        }
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
