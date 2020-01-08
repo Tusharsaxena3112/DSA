246b04664f5fe32df2de9cf9776d36fd8d67f7a2
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-08 19:13:06 +0530

    adding insert method

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -3,9 +3,15 @@ package arrays;
 public class Arrays {
     private int length;
     private int[] array;
+    private int index;
 
     public Arrays(int length) {
         this.length = length;
         array = new int[length];
+    }
+
+    public void insert(int element) {
+        array[index] = element;
+        index++;
     }
 }
