210c09fe2fbf4090a31f8bff4883e1caa0732e71
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-08 19:04:47 +0530

    adding Arrays.java

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Arrays {
+    private int length;
+    private int[] array;
+
+    public Arrays(int length) {
+        this.length = length;
+        array = new int[length];
+    }
+}
