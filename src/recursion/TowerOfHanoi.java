dc96c711f4759a10d1f641e0049abaab924b88d0
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-01-22 16:42:31 +0530

    completing TowerOfHanoi.java

--- src/recursion/TowerOfHanoi.java
+++ src/recursion/TowerOfHanoi.java
@@ -1,5 +1,20 @@
 package recursion;
 
 public class TowerOfHanoi {
+    public static void main(String[] args) {
+        TowerOfHanoi towerOfHanoi = new TowerOfHanoi();
+        towerOfHanoi.tower(3, 'A', 'B', 'C');
+    }
 
+    private void tower(int n, char beg, char aux, char end) {
+        if (n >= 1) {
+            tower(n - 1, beg, end, aux);
+            System.out.printf("disk moved from %c to %c\n", beg, end);
+            tower(n - 1, aux, beg, end);
+            //System.out.printf("disk moved from %c to %c\n",beg,end);
+        }
+//        else{
+//            System.out.printf("Disk moved from %c to %c\n",beg,end);
+//        }
+    }
 }
