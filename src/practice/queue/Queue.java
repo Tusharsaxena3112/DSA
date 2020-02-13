698cde3428d012ef07eb677c87ce5a2b07151344
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-13 22:03:29 +0530

    adding constructor

--- src/practice/queue/Queue.java
+++ src/practice/queue/Queue.java
@@ -1,5 +1,18 @@
 package practice.queue;
 
 public class Queue {
+    private int front;
+    private int rear;
+    private int maxSize;
+    private int[] queue;
+
+    public Queue(int maxSize) {
+        queue = new int[maxSize];
+        this.front = 0;
+        this.rear = 0;
+    }
+
+
+
 
 }
