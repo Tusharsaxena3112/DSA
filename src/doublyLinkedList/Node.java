5c469de5dd712b8a2f16c0be313856583ce2ac0a
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-17 16:22:11 +0530

    adding constructor

--- src/doublyLinkedList/Node.java
+++ src/doublyLinkedList/Node.java
@@ -1,5 +1,19 @@
 package doublyLinkedList;
 
 public class Node {
+    private Node next;
+    private Node previous;
+    private int data;
 
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node node) {
+        this.next = node;
+    }
 }
