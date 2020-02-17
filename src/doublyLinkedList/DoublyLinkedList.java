017139c069b6d2564c6d37db6417a3337df52d21
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-17 16:27:37 +0530

    adding addFirst() method in DoublyLinkedList.java

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,17 @@
 package doublyLinkedList;
 
 public class DoublyLinkedList {
+    private Node first;
+    private Node last;
 
+    public void addFirst(int data) {
+        Node node = new Node(data);
+        if (first == null) {
+            first = last = node;
+        } else {
+            node.setNext(first);
+            first.setPrevious(node);
+            first = node;
+        }
+    }
 }
