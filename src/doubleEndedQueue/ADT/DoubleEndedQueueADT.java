cba2049646923cf9bfe582bf80e5ad58fb63fe79
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-24 16:24:30 +0530

    completing ADT

--- src/doubleEndedQueue/ADT/DoubleEndedQueueADT.java
+++ src/doubleEndedQueue/ADT/DoubleEndedQueueADT.java
@@ -1,5 +1,11 @@
 package doubleEndedQueue.ADT;
 
 public interface DoubleEndedQueueADT {
+    void enqueueRear(int item);
 
+    void enqueueFront(int item);
+
+    int dequeueRear();
+
+    int dequeueFront();
 }
