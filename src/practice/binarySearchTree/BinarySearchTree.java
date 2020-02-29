46aa47a449af7ff55eaadf8c12b1e55a0b652506
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-29 21:01:42 +0530

    adding add()

--- src/practice/binarySearchTree/BinarySearchTree.java
+++ src/practice/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,33 @@
 package practice.binarySearchTree;
 
 public class BinarySearchTree {
+    private Node root;
 
+    public boolean add(int element) {
+        Node node = new Node(element);
+        boolean response = false;
+        if (root == null) {
+            root = node;
+            response = true;
+        } else {
+            var temp = root;
+            Node parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (temp.getData() <= node.getData()) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+            if (node.getData() <= parent.getData()) {
+                parent.setLeft(node);
+                response = true;
+            } else {
+                parent.setRight(node);
+                response = true;
+            }
+        }
+        return response;
+    }
 }
