8bc64e20344ef9166231cac03e3da17ced5504f3
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-02-29 10:50:01 +0530

    adding enqueue()

--- src/practice/priorityQueue/PriorityQueue.java
+++ src/practice/priorityQueue/PriorityQueue.java
@@ -2,11 +2,22 @@ package practice.priorityQueue;
 
 public class PriorityQueue {
     int maxSize;
-    private int element;
     private int[] queue;
+    int front = 0;
+    int rear = 0;
 
     public PriorityQueue(int maxSize) {
         this.maxSize = maxSize;
         queue = new int[maxSize];
     }
+
+    public void enqueue(int element) {
+        if (rear < maxSize) {
+            queue[rear] = element;
+            rear++;
+        } else {
+            System.out.println("Queue is full");
+        }
+    }
+
 }
