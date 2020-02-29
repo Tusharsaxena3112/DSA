19027b0278aa83f01a5507f1b37d4598dc0e3475
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-03-01 00:31:35 +0530

    adding postOrder(),preOrder(),inOrder()

--- src/practice/binarySearchTree/BinarySearchTree.java
+++ src/practice/binarySearchTree/BinarySearchTree.java
@@ -14,7 +14,7 @@ public class BinarySearchTree {
             Node parent = null;
             while (temp != null) {
                 parent = temp;
-                if (temp.getData() <= node.getData()) {
+                if (node.getData() <= temp.getData()) {
                     temp = temp.getLeft();
                 } else {
                     temp = temp.getRight();
@@ -29,5 +29,42 @@ public class BinarySearchTree {
             }
         }
         return response;
+    }
+
+    public void displayPreOrder() {
+        preOrder(root);
+    }
+
+    public void displayInOrder() {
+
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.getData());
+        preOrder(node.getLeft());
+        preOrder(node.getRight());
+    }
+
+    private void inOrder(Node node) {
+        if (root == null) {
+            return;
+        } else {
+            inOrder(node.getLeft());
+            System.out.print(node.getData() + " ");
+            inOrder(node.getRight());
+        }
+    }
+
+    private void postOrder(Node node) {
+        if (root == null) {
+            return;
+        } else {
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            System.out.print(node.getData() + " ");
+        }
     }
 }
--- src/practice/binarySearchTree/Main.java
+++ src/practice/binarySearchTree/Main.java
@@ -1,5 +1,14 @@
 package practice.binarySearchTree;
 
 public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(10);
+        bst.add(2);
+        bst.add(50);
+        bst.add(23);
+        bst.add(5);
 
+        bst.display();
+    }
 }
