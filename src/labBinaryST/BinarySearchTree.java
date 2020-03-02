a1746d0d70d2e0ac096b5510daba9d99346af3c5
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-03-02 16:22:16 +0530

    adding insert()

--- src/labBinaryST/BinarySearchTree.java
+++ src/labBinaryST/BinarySearchTree.java
@@ -1,5 +1,34 @@
 package labBinaryST;
 
 public class BinarySearchTree {
+    private TreeNode root;
 
+    public void insert(int data) {
+        TreeNode treeNode = new TreeNode(data);
+        if (isEmpty()) {
+            root = treeNode;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (treeNode.getData() <= temp.getData()) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+            if (parent.getData() <= treeNode.getData()) {
+                parent.setLeft(treeNode);
+            } else {
+                parent.setRight(treeNode);
+            }
+        }
+
+    }
+
+
+    public boolean isEmpty() {
+        return root == null;
+    }
 }
