8a339a8f1728df169c8d07c08bd949246be83edf
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-03-26 14:28:11 +0530

    completing LinearSearch.java

--- src/searchingTechniques/LinearSearch.java
+++ src/searchingTechniques/LinearSearch.java
@@ -1,5 +1,20 @@
 package searchingTechniques;
 
 public class LinearSearch {
-
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 56, 7};
+        int searchElement = 5;
+        boolean response = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == searchElement) {
+                response = true;
+                break;
+            }
+        }
+        if (response == true) {
+            System.out.println("Present");
+        } else {
+            System.out.println("Not Present");
+        }
+    }
 }
