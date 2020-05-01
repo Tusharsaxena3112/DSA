7a92dfa1597b82797a33067cae36926ff4129429
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-05-01 21:13:50 +0530

    adding addEdge() method

--- src/graph/MyGraph.java
+++ src/graph/MyGraph.java
@@ -2,12 +2,27 @@ package graph;
 
 public class MyGraph {
     private int maxNodes;
-    private int[][] adjancencyMatrix;
+    private int[][] adjacencyMatrix;
 
     public MyGraph(int maxNodes) {
         this.maxNodes = maxNodes;
-        adjancencyMatrix = new int[maxNodes][maxNodes];
+        adjacencyMatrix = new int[maxNodes][maxNodes];
     }
 
+    public boolean addEdge(int node1, int node2) {
+        boolean response = false;
+        if (node1 < maxNodes && node2 < maxNodes) {
+            adjacencyMatrix[node1][node2] = 1;
+            adjacencyMatrix[node2][node1] = 1;
+            response = true;
+        } else {
+            System.out.println("Cannot add Edge..");
+        }
+        return response;
+    }
 
+    public boolean removeEdge(int node1, int node2) {
+        boolean response = false;
+        return response;
+    }
 }
